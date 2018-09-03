20f29084e22db60c91f4fd47970f7fe621f316e4
techfanatic <heroward@msn.com>
2018-09-03 13:00:31 +0800

    add union pay sign

---
+++ union_pay.sikuli/union_pay.py
@@ -0,0 +1,14 @@
+click("1535950582554.png")
+
+sleep(5)
+
+sign = "sign.png"
+
+if exists(sign):
+    click(sign)
+    sleep(2)
+    click("1535950686472.png")
+
+    sleep(5)
+
+    click("1535950758630.png")
